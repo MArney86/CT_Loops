076454add05bdc1add19f14ec64844af751b118e
MArney86 <matthew.h.arney.1986@gmail.com>
2024-10-24 05:35:05 -0500

    updated for current assignments

--- 1_The_Range_Riddle.py
+++ 1_The_Range_Riddle.py
@@ -3,5 +3,6 @@
 import random
 moods = ["happy", "sad", "energetic", "calm", "playful"]#A list of moods 
 weekdays = ['Monday', "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"] #Days of the week for print function later
+
 for x in range(7):#looping through the days of the week using range()
     print(f"On {weekdays[x]} you were felling {random.choice(moods)}") #randomly choosing the mood when printing the mood for that day of the week
